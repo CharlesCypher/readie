3613afc4feec121ef901288af5504a3bf7cc8f87
CharlesCypher <99364836+CharlesCypher@users.noreply.github.com>
2023-07-16 21:17:47 +0100

    updated

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,8 @@
-import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../config/firebase";
 import { X } from "phosphor-react";
 
 export default function Menu({ isOpen, setIsOpen }) {
@@ -7,14 +10,16 @@ export default function Menu({ isOpen, setIsOpen }) {
   useEffect(() => {
     setIsOpen(false);
   }, [setIsOpen, location]);
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
   return (
     <>
       {isOpen && (
         <div className="fixed h-full top-0 left-0 w-3/5 bg-gray-900 z-50 md:w-4/12 lg:w-3/12">
           <div className="relative w-full h-full z-50">
             <ul className="list-none">
-              <li className="font-bold text-lg text-white text-center my-8">
-                <Link to={"/login"}>Login</Link>
+              <li className="font-bold text-lg text-white text-center my-8" onClick={() => navigate(currentUser ? signOut(auth) : "/login")}>
+                <Link>{currentUser ? "Sign out" : "Login"}</Link>
               </li>
               <li className="font-bold text-lg text-white text-center my-8">
                 <Link to={"/createpost"}>Make a Post</Link>
